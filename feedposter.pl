e3bace69ce84b5739320bb1ad0da4f6001ce6843
Dennis <dennis@localhost>
2012-10-29 11:57:10 +0200

    Added feedposter.pl

---
+++ feedposter.pl
@@ -0,0 +1,16 @@
+#!/usb/bin/perl -w
+# 
+# feedposter.pl - a script that does 2 things:
+#     1) process RSS feed and get all articles that have specified keywords;
+#     2) get HTML-only version of an article
+#     3) post to Wordpress blog
+#
+
+use strict;
+
+# TODO - read config
+
+# TODO - for each RSS feed, get feed articles
+# TODO - for each article - get article text
+# TODO - post article text to Wordpress
+#
